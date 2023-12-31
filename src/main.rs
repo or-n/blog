57f5a44b1387d3a0ec378defc4bc32e049bf15a5
or-n <kulombowie@gmail.com>
2023-12-31 23:24:02 +0100

    simplify main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,16 @@ use warp::Filter;
 pub mod post;
 pub mod util;
 
-fn html() -> route!(impl warp::Reply) {
-    warp::fs::dir("html")
-}
-
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    warp::serve(html().or(api()))
-        .run(([127, 0, 0, 1], 3030))
-        .await;
+    warp::serve(
+        warp::fs::dir("html")
+            .or(post::api())
+            .or(warp::get().map(handle_end)),
+    )
+    .run(([127, 0, 0, 1], 3030))
+    .await;
     Ok(())
-}
-
-fn api() -> route!(impl warp::Reply) {
-    let end = warp::get().map(handle_end);
-    post::api().or(end)
 }
 
 fn handle_end() -> impl warp::Reply {
